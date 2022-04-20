d3d9e16061ddabcd0c596cc375c884a47577a29a
risa9528 <85784298+risa9528@users.noreply.github.com>
2022-04-20 23:27:07 +0900

    ios

---
+++ practice/Hellowold/Hellowold/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hellowold
+//
+//  Created by 新谷莉彩 on 2022/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
